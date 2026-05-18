c599b730af493d1805b8866e10e3694a84f30084
Kevin Cameron <kevinacameron@hotmail.com>
2026-05-17 21:59:09 -0700

    better demo

--- apps/website/src/routes/demo/+page.svelte
+++ apps/website/src/routes/demo/+page.svelte
@@ -5,21 +5,29 @@
   <script src="https://unpkg.com/simple-nps@0.0.5/dist/simple-nps.iife.js" defer></script>
 </svelte:head>
 
+<div class="flex">
   <h1>Simple NPSDemo</h1>
   <p>Look in the bottom corner.</p>
 
-  <button onclick={() => window.nps.init()}>
-    re-init NPs
+  <button onclick={() => window.nps.init()} class="btn secondary">
+    Re-initialize NPS
   </button>
+</div>
 
 <style>
+  .flex {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+  }
+
   h1 {
-    text-align: center;
     margin-bottom: 1rem;
   }
 
   p {
-    text-align: center;
     color: #666;
     margin-bottom: 3rem;
   }
